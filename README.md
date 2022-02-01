94c32c16a01cb7f1af3af840d151c0867db9bdfb
rwagh <rahul.wagh@crosskey.fi>
2022-02-01 22:30:06 +0200

    my other app commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rest-webservice-springboot
+
+docker build -t rest-webservice -f Dockerfile  .
+
+docker tag rest-webservice rahulwagh17/rest-webservice:rest-webservice
+
+docker run -p 8080:9090 -t rahulwagh17/rest-webservice:rest-webservice
+
+
+docker run -p 9090:9090 -t rest-webservice -f Dockerfile  .
---
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -0,0 +1,13 @@
+package com.jhooq.Jhooqk8s.ws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JhooqDockerDemoController {
+
+    @GetMapping("/makeOtherAppCall")
+    public String hello() {
+        return "Message from other app";
+    }
+}
